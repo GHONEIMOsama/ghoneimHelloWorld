e5a7e436f242aed9ce1959e891e141c1bfdbe1fd
ghoneim <osama.ghoneim.etu@univ-lille.fr>
2020-10-09 13:36:59 +0200

    FEAT: add mvn project

---
+++ src/main/java/ghoneim/App.java
@@ -0,0 +1,13 @@
+package ghoneim;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
